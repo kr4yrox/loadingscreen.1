008d08145a7ae81319bdee577e72d65e03b688de
kr4yrox <126266327+kr4yrox@users.noreply.github.com>
2023-07-21 02:24:25 +0200

    Add files via upload

---
+++ YBN_loadingscreen/assets/scripts/config.js
@@ -0,0 +1,34 @@
+Config = {}; // Don't touch
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "youtube", icon: "assets/media/icons/youtube.png", link: "https://www.youtube.com/channel/UCC1zKL5kqlfgwMpQKcKzokA"},
+    {name: "site", icon: "assets/media/icons/site.png", link: "https://sites.google.com/view/hexagonefrancerp/accueil"},
+    {name: "tiktok", icon: "assets/media/icons/tiktok.png", link: "https://www.tiktok.com/@hexagonefrancerp"},
+    {name: "twitter", icon: "assets/media/icons/twitter.png", link: "https://twitter.com/Focustwt"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+
+// Team list
+Config.Staff = [
+    {name: "fondateur", description: "fondateur du serveur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/alt.png"},
+    {name: "fondateur", description: "fondateur du serveur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/tab.png"},
+    {name: "admin", description: "Axel admin sur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/f2.png"},
+    {name: "admin", description: "Alexis admin sur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/f1.png"},
+    {name: "admin", description: "Fabio Admin sur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/1234.png"},
+    {name: "admin", description: "Mickael Admin sur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/f4.png"},
+    {name: "moderateur", description: "bernard est moderateur sur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/f6.png"},
+    {name: "moderateur", description: "yanis est moderateur sur Hexagone France RP", color: "#ff1313", image: "assets/media/touches/f11.png"},
+    {name: "Figures Motos", description: "Permet de faire des figures quand vous êtes en motos", color: "#ff1313", image: "assets/media/touches/f11_d.png"},
+    {name: "Parler en Radio", description: "Permet de parler en radio", color: "#ff1313", image: "assets/media/touches/unknown_3.png"},
+    {name: "Ceinture de Sécurité", description: "Permet de mettre ou enlever votre ceinture", color: "#ff1313", image: "assets/media/touches/k.png"},
+];
+
+// Categories
+Config.Categories = [
+    {label: "Le Staff", default: true}
+];
+
